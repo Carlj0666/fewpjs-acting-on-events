a8651b0df1124f8e05fdd048b8e0fa47773f2b5a
Carlj0666 <carlj1881@yahoo.com>
2021-03-20 13:30:03 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "green";
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
